89f007b54e43cdcbbc83dd97dc32963d9996817e
Christian Serrano <nano561@icloud.com>
2019-09-15 20:38:46 -0400

    Updated logic to store Json file as a string
    also you can have multiple counters,
    pass in the counter name in the function => APICounter('name')
    it will default to 'counterAPI'

--- APICounter.js
+++ APICounter.js
@@ -1,29 +1,56 @@
-/* Count # of API request*/
-export const APICounter = () => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const currentDay = new Date().getDate();
-    const counterAPIDate = parseInt(localStorage.getItem('counterAPIDate'));
-    const counterAPIMonth = parseInt(localStorage.getItem('counterAPIMonth'));
-    const counterAPIYear = parseInt(localStorage.getItem('counterAPIYear'));
-    let counterAPI = parseInt(localStorage.getItem('counterAPI'));
+/* Count # of API request
 
-    const restCounterAPI = () => {
-        localStorage.setItem('counterAPI', '1');
-        localStorage.setItem('counterAPIDate', `${currentDay}`);
-        localStorage.setItem('counterAPIMonth', `${currentMonth}`);
-        localStorage.setItem('counterAPIYear', `${currentYear}`);
+* cls = current local storage
+* cy = current year
+* cm = current month
+* cdy = current day
+* cn = counter name
+* clsli = current local storage last item - meaning the last object in the array
+* */
+
+
+const APICounter = (cn = 'counterAPI') => {
+    const cy = new Date().getFullYear();
+    const cm = new Date().getMonth();
+    const cdy = new Date().getDate();
+
+    //this stores the object as a string on the end user computer
+    const storeObject = (cls) => {
+        console.log(`Daily API calls: ${cls.data[cls.data.length-1].counterAPI}`);
+        localStorage.setItem(cn, JSON.stringify(cls));
     };
 
-    if (parseInt(localStorage.getItem('counterAPI')) < 1) {
-        restCounterAPI();
-        console.log(`First API Call 😋`)
-    } else if (currentYear == counterAPIYear && currentMonth == counterAPIMonth && currentDay == counterAPIDate) {
-        counterAPI += 1;
-        localStorage.setItem('counterAPI', `${counterAPI}`);
-        alert(`API Counter: ${counterAPI}`);
+    if (localStorage.getItem(cn) === null) {
+        let cls = {
+            data: [
+                {
+                    counterAPI: 1,
+                    counterYear: cy,
+                    counterMonth: cm,
+                    counterDay: cdy
+                }
+            ]
+        };
+        storeObject(cls);
     } else {
-        restCounterAPI();
-        alert(`First API Call of the day 😎`);
+        let cls = JSON.parse(localStorage.getItem(cn));
+        let clsli = cls.data[cls.data.length - 1];
+
+        if (parseInt(clsli.counterYear) === cy &&
+            parseInt(clsli.counterMonth) === cm &&
+            parseInt(clsli.counterDay) === cdy) {
+            clsli.counterAPI = parseInt(clsli.counterAPI) + 1;
+            storeObject(cls);
+        } else {
+            cls.data[cls.data.length] = {
+                counterAPI: 1,
+                counterYear: cy,
+                counterMonth: cm,
+                counterDay: cdy,
+            };
+            storeObject(cls);
+        }
     }
 };
+
+APICounter();
